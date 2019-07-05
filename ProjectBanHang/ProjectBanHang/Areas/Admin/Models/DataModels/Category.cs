6273ee69069b27cbbed3e4a6ebf2f34ebcb821ef
hien_office <hienoffice.@gmail.com>
2019-07-05 17:37:08 +0700

    update code

--- ProjectBanHang/ProjectBanHang/Areas/Admin/Models/DataModels/Category.cs
+++ ProjectBanHang/ProjectBanHang/Areas/Admin/Models/DataModels/Category.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +9,12 @@ namespace ProjectBanHang.Areas.Admin.Models.DataModels
 {
     public class Category
     {
+        [Key]
+        [Required]
+
         public int Id { get; set; }
+        [Required]
+        [DisplayName("Tên hãng")]
         public string Name { get; set; }
 
         //Thuộc tính liên kết 
