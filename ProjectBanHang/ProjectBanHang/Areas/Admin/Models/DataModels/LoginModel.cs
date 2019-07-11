7ff76b71d2d76baa3a44ad9030c670620beaf89e
hien_home <hienhome@gmail.com>
2019-07-11 19:08:50 +0700

    adding a change from the feature branch

---
+++ ProjectBanHang/ProjectBanHang/Areas/Admin/Models/DataModels/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectBanHang.Areas.Admin.Models.DataModels
+{
+    public class LoginModel
+    {
+    }
+}
